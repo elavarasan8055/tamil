65c0b1d8421d1178b7baf0330c8410e4c0ed28ac
elavarasan <elavarasan8055@gmail.com>
2015-03-10 18:23:47 +0530

    2nd update

---
+++ tamil.py
@@ -0,0 +1,12 @@
+vowel = {'a':u'\u0b85','aa':u'\u0b86','i':u'\u0b87','ii':
+    u'\u0b88','u':u'\u0b89','uu':u'\u0b8a','e':u'\u0b8e',
+    'ee':u'\u0b8f','ai':u'\u0b90','o':u'\u0b92','oo':u'\u0b93',
+    'au':u'\u0b94'}
+
+consonant = {'ka':u'\u0b95','nga':u'\u0b99','ca':u'\u0b9a',
+    'ja':u'\u0b9c','nya':u'\u0b9e','tta':u'\u0b9f','nna':u'\u0ba3'
+    'ta':u'\u0ba4','na':u'\u0ba8','nnna':u'\u0ba9','pa':u'\u0baa',
+    'ma':u'\u0bae','ya':u'\u0baf','ra':u'\u0bb1':'la',}
+val = raw_input()
+
+print vowel[val]
